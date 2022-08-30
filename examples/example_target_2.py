3ef67aa55769dbbc99357f2ed27698da66943d52
TheSolipsist <57457393+TheSolipsist@users.noreply.github.com>
2022-08-30 07:38:30 +0300

    added __init__ + example

---
+++ examples/example_target_2.py
@@ -0,0 +1,34 @@
+import pymeleon as pym
+import pygrank as pg
+import networkx as nx
+
+
+def list2dict(x: list) -> dict:
+    return {v: 1 for v in x}
+
+
+def signal2graph(x: pg.GraphSignal) -> nx.Graph:
+    return x.graph
+
+
+def str2list(x: str) -> list:
+    return [x]
+
+
+def concat(x: list, y: list) -> list:
+    return x + y
+
+
+viewer = pym.DSL(
+    pym.autorule(signal2graph),
+    pym.autorule(list2dict),
+    pym.autorule(str2list),
+    pym.autorule(concat),
+    pym.Rule(pym.parse({"graph": nx.Graph, "data": dict}),
+             pym.parse("pg.to_signal(graph, data)", {"pg.to_signal": ("noinput", pg.GraphSignal)})),
+) >> pym.GeneticViewer({"pg": pg}, use_pretrained=True, hyperparams={"num_epochs": 1000}, device_str="cuda")
+
+G = nx.Graph()
+G.add_edge("node_a", "node_b")
+result = viewer(G, "node_a", "node_b") >> pym.parse(pg.GraphSignal)
+print(result)
