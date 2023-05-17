296afc425f734da4b6b8c14f7748f5f199f4725c
TheSolipsist <57457393+TheSolipsist@users.noreply.github.com>
2023-05-17 10:44:02 +0300

    results json output

--- for_text/algorithmic_comparison/compare_algorithms.py
+++ for_text/algorithmic_comparison/compare_algorithms.py
@@ -1,38 +1,46 @@
 """
-Create 4-5 DSLs and come up with 5 examples for each, ascending complexity
-Compare algorithms with and without neural net for each DSL and measure mean
-For each example write an explanation for what it does
-Measure dataset generation time, DSL training time, mean inference time (100 iterations or sth, check how many of them find the solution)
-Table, on the left problem name, 2 columns describing search time and results without neural net, 
+Algorithmic comparison module for pymeleon
 """
-from for_text.algorithmic_comparison import pygrank_test, numpy_test, graph_test, string_test
+import json
+import pathlib
 
-modules = [pygrank_test,
-           graph_test, 
-           numpy_test,
-           string_test]
+from for_text.algorithmic_comparison import (graph_test, numpy_test,
+                                             pygrank_test, string_test)
 
+modules = {
+    mod: mod.__name__.split(".")[-1]
+    for mod in (pygrank_test, graph_test, numpy_test, string_test)
+}
 n = 100
 n_scen = 5
-for fitness_str in ("neural_random", "heuristic"):
-    for mod in modules:
-        print(fitness_str, mod.__name__)
+fitness_funcs = ("neural_random", "heuristic", "random")
+json_dict = {
+    "n": n,
+}
+for fitness_str in fitness_funcs:
+    json_dict[fitness_str] = {}
+    for mod, mod_name in modules.items():
         found = [0] * n_scen
         total_times = [0] * n_scen
         total_iterations = [0] * n_scen
-        for _ in range(n):
+        for j in range(n):
+            print(f"\r{' ' * 100}\rTesting fitness function: {fitness_str}, module: {mod_name}, iter: {j + 1} / {n}", end="")
             results = mod.test(fitness_str)
             for i, result in enumerate(results):
                 if result is not False:
                     found[i] += 1
                     total_times[i] += result[1]
                     total_iterations[i] += result[0][1]
-        with open(f"results/experiments/CGP/{fitness_str}_{mod.__name__}", "w") as f:
-            f.write(f"THIS IS {mod.__name__}\n\n")
-            for i in range(n_scen):
-                f.write(f"Scenario {i + 1}:\n")
-                f.write(f"\tAccuracy: {found[i] / n}\n")
-                f.write(f"\tIterations: {total_iterations[i] / max(found[i], 1)}\n")
-                f.write(f"\tTime: {total_times[i] / max(found[i], 1)}\n")
-                
-print("DONE")
+        json_dict[fitness_str][mod_name] = {
+            i + 1: {
+                "accuracy": found[i] / n,
+                "avg_iter_n": total_iterations[i] / found[i] if found[i] > 0 else "N/A",
+                "avg_time": total_times[i] / found[i] if found[i] > 0 else "N/A",
+            }
+            for i in range(n_scen)
+        }
+print()
+file_p = pathlib.Path(f"results/experiments.json")
+file_p.parent.mkdir(parents=True, exist_ok=True)
+with file_p.open(mode="w", encoding="utf-8") as json_file:
+    json.dump(json_dict, json_file, indent=4)
